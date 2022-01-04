3c6fd4b59acbafa1bc80fcd5991f0fe51cf3f3d1
Paulo Lyra-Jr <pj_ibm@hotmail.com>
2022-01-04 18:23:22 -0300

    update

--- dags/dag1.py
+++ dags/dag1.py
@@ -12,14 +12,14 @@ default_args = {
 'owner'                 : 'airflow',
 'description'           : 'Crawler using Scrapy Python',
 'depend_on_past'        : False,
-'start_date'            : datetime(2022, 1, 3),
+'start_date'            : datetime(2022, 1, 4),
 'email_on_failure'      : False,
 'email_on_retry'        : False,
 'retries'               : 1,
 'retry_delay'           : timedelta(minutes=5)
 }
 
-with DAG('go_spider_operator_dag', default_args=default_args, schedule_interval="* /5 * * * *", catchup=False) as dag:
+with DAG('go_spider_operator_dag', default_args=default_args, schedule_interval='*/5 * * * *', catchup=False) as dag:
     start_dag = DummyOperator(
         task_id='start_dag'
         )
@@ -28,16 +28,31 @@ with DAG('go_spider_operator_dag', default_args=default_args, schedule_interval=
         task_id='end_dag'
         )        
 
-    t1 = PythonOperator(
-        task_id='docker_command_go_spider',
-        image='docker_image_task',
-        container_name='Scrapy_spider',
-        api_version='auto',
-        auto_remove=True,
-        command="python go_spider.py",
-        docker_url="unix://var/run/docker.sock",
-        network_mode="bridge"
-        )
+    t1 = BashOperator(
+        task_id='move_to_directory',
+        bash_command='cd /opt/airflow/dags/tasks',
+    )
+    
+    t2 = BashOperator(
+        task_id='go_spider_command',
+        bash_command='python go_spider.py',
+    )
+    
+    #t2 = PythonOperator(
+    #    task_id='spider-go',
+    #    python_callable=main
+    #    )
+    
+    #t1 = DockerOperator(
+    #    task_id='docker_command_go_spider',
+    #    image='go_spider_image',
+    #    container_name='go_spider_image',
+    #    api_version='auto',
+    #    auto_remove=True,
+    #    command="echo done",
+    #    docker_url="unix://var/run/docker.sock",
+    #    network_mode="bridge"
+    #    )
         
     start_dag >> t1 
 
