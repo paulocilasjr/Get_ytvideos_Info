24870ee373392d5efda4a20d72f0fe2dbe50cfc6
Paulo Lyra-Jr <pj_ibm@hotmail.com>
2022-01-17 22:13:31 -0400

    Update - regex and cleanning

--- dags/tasks/getvideos/spiders/getvideos_spider.py
+++ dags/tasks/getvideos/spiders/getvideos_spider.py
@@ -9,29 +9,29 @@ class GetvideosSpiderSpider(scrapy.Spider):
     start_urls = ['https://www.youtube.com/results?search_query=ish+tecnologia&sp=EgQIBRAB']
     
     def parse(self, response):
-        pattern = re.compile('[^.]* ytInitialData [^;]*')
-        allDataJson = response.css('script::text').re_first(pattern).replace("var ytInitialData = ", "")
+        
+        pattern = re.compile('[^.]* ytInitialData [^<]*')
+        allDataJson = response.css('script::text').re_first(pattern).replace("var ytInitialData = ", "")[:-1]
         allDataDict = json.loads(allDataJson)
+        
         ListWithVideosInfo = allDataDict["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"]
         
         videoNumber = 1
         videosInfo = {}
         for index in range(len(ListWithVideosInfo)):
-            videosInfo["Video %d"%videoNumber] = []
-            videosInfo["Video %d"%videoNumber].append({
-            "Title" : ListWithVideosInfo[index]["videoRenderer"]["title"]["runs"][0]["text"],
-            "URL" : "www.youtube.com" + ListWithVideosInfo[index]["videoRenderer"]["navigationEndpoint"]["commandMetadata"]["webCommandMetadata"]["url"]
-            })
+            videosInfo["Video %d"%videoNumber] = {
+                "Title" : ListWithVideosInfo[index]["videoRenderer"]["title"]["runs"][0]["text"],
+                "URL" : "www.youtube.com" + ListWithVideosInfo[index]["videoRenderer"]["navigationEndpoint"]["commandMetadata"]["webCommandMetadata"]["url"]   
+            }
             videoNumber += 1
-        print(videosInfo)
         
-        #videosInfoJson = json.dumps(videosInfo, indent=4)
-        #print(videosInfoJson)
+        videosInfoJson = json.dumps(videosInfo, indent=4, ensure_ascii=False)
+        print(videosInfoJson)
 
         #<<<<IF YOU WANT SAVE A FILE AT THE END>>>
-        #page = response.url.split("/")[-2]
-        #filename = "ytvideos_Info_Result.json"
-        #with open(filename, 'w') as f:
-        #    f.write(videosInfoJson)
-        #self.log(f'Saved file {filename}')
+        page = response.url.split("/")[-2]
+        filename = "ytvideos_Info_Result.json"
+        with open(filename, 'w', encoding='utf8') as f:
+            f.write(videosInfoJson)
+        self.log(f'Saved file {filename}')
         
